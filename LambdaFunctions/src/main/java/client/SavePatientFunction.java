f86887abe3997935e9e5df15040325f915f95003
devxope <devxope@gmail.com>
2020-03-22 15:26:58 +0100

    added detailed logic for input values via json

--- LambdaFunctions/src/main/java/client/SavePatientFunction.java
+++ LambdaFunctions/src/main/java/client/SavePatientFunction.java
@@ -6,6 +6,9 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.commons.lang3.BooleanUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,41 +19,42 @@ import static java.util.Objects.nonNull;
 /**
  * Handler for requests to Lambda function.
  */
-public class SavePatientFunction implements RequestHandler<PatientRequest, Object> {
+public class SavePatientFunction implements RequestHandler<Object, Object> {
 
     private static final String DYNAMODB_TABLE_NAME = "patient_data";
 
-    public Map<String, String> handleRequest(final PatientRequest input, final Context context) {
+    public Map<String, String> handleRequest(final Object input, final Context context) {
         final AmazonDynamoDB db = AmazonDynamoDBClientBuilder.defaultClient();
         final Map<String, AttributeValue> data = new HashMap<>();
         final Map<String, String> response = new HashMap<>();
         final UUID uuid = UUID.randomUUID();
+        context.getLogger().log(String.valueOf(input));
+        final JSONObject inputData = (JSONObject) input;
 
         response.put("clientId", uuid.toString());
 
         data.put("patient_id", new AttributeValue().withS(uuid.toString()));
 
-        if (nonNull(input.contact)) {
-            data.put("contact", new AttributeValue().withBOOL(Boolean.valueOf(input.contact)));
+        if (nonNull(inputData.get("contact"))) {
+            data.put("contact", new AttributeValue().withBOOL(Boolean.valueOf(inputData.get("contact").toString())));
         }
-        if (nonNull(input.coughing)) {
-            data.put("coughing", new AttributeValue().withBOOL(Boolean.valueOf(input.coughing)));
+        if (nonNull(inputData.get("coughing"))) {
+            data.put("coughing", new AttributeValue().withBOOL(BooleanUtils.toBoolean(inputData.get("coughing").toString())));
         }
-        if (nonNull(input.fever)) {
-            data.put("fever", new AttributeValue().withBOOL(Boolean.valueOf(input.fever)));
+        if (nonNull(inputData.get("fever"))) {
+            data.put("fever", new AttributeValue().withBOOL(Boolean.valueOf(inputData.get("fever").toString())));
         }
-        if (nonNull(input.otherSymptoms)) {
-            data.put("otherSymptoms", new AttributeValue().withBOOL(Boolean.valueOf(input.otherSymptoms)));
+        if (nonNull(inputData.get("otherSymptoms"))) {
+            data.put("otherSymptoms", new AttributeValue().withBOOL(Boolean.valueOf(inputData.get("otherSymptoms").toString())));
         }
-        if (nonNull(input.regionAtRisk)) {
-            data.put("regionAtRisk", new AttributeValue().withBOOL(Boolean.valueOf(input.regionAtRisk)));
+        if (nonNull(inputData.get("regionAtRisk"))) {
+            data.put("regionAtRisk", new AttributeValue().withBOOL(Boolean.valueOf(inputData.get("regionAtRisk").toString())));
         }
-        if (nonNull(input.phoneNumber)) {
-            data.put("phoneNumber", new AttributeValue().withS(input.phoneNumber));
+        if (nonNull(inputData.get("phoneNumber"))) {
+            data.put("phoneNumber", new AttributeValue().withS(inputData.get("phoneNumber").toString()));
         }
 
         db.putItem(new PutItemRequest(DYNAMODB_TABLE_NAME, data));
-
         return response;
     }
 }
